2f707366724655b7cf371c48ad2ea4fdba5dd6ed
kamal242 <kamalnayan242@gmail.com>
2022-07-20 17:29:08 +0530

    day10

--- map_.py
+++ map_.py
@@ -308,3 +308,14 @@ df1 = df.select(df.dept,df.salary)\
 
 ##GroupBy API
 df2 = df1.select(df1.dept,df1.salary).groupBy(df1.dept).avg().show()
+
+data = (("James","Sales","NY",9000,34),
+("Alicia","Sales","NY",8600,56),
+("Robert","Sales","CA",8100,30),
+("Lisa","Finance","CA",9000,24),
+("Deja","Finance","CA",9900,40),
+("Sugie","Finance","NY",8300,36),
+("Ram","Finance","NY",7900,53),
+("Kyle","Marketing","CA",8000,25),
+("Reid","Marketing","NY",9100,50)
+)
